b61a8d327c0ca9482752bbd4df54b432b4b8f75c
WizyX <156693069+WizyX77@users.noreply.github.com>
2024-08-10 18:31:25 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,14 +6,11 @@ import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Wizyx",
-  description: " Hi, I’m @_wizyx I'm currently learning I am currently living in Adana, Turkey.",
+  description: " Hi, I’m @_wizyx",
   themeColor: "#8B5CF6",
   robots: "index, follow",
   viewport: "width=device-width, initial-scale=1, maximum-scale=1",
   icons: "https://cdn.discordapp.com/attachments/1266051388962640056/1271216520558674051/Discord-Profil.jpg?ex=66b68824&is=66b536a4&hm=c38b2b5ae38c9b6c82ed3cd59906089f8d55b9b92fc2aa189a8bd5958d0bbc72&",
-  openGraph: {
-    images: "https://cdn.discordapp.com/attachments/1266051388962640056/1271216520558674051/Discord-Profil.jpg?ex=66b68824&is=66b536a4&hm=c38b2b5ae38c9b6c82ed3cd59906089f8d55b9b92fc2aa189a8bd5958d0bbc72&",
-  },
 };
 
 export default function RootLayout({
