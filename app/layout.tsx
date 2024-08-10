26b9df84294e8a1473f3bc421eb03af7322c8348
WizyX <156693069+WizyX77@users.noreply.github.com>
2024-08-10 18:36:20 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,11 +6,14 @@ import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Wizyx",
-  description: " Hi, I’m @_wizyx",
+  description: "**» Bot / Web Developer & Content Creator. Professional Pubg Mobile Player, S2G Here Academy Leader! «**",
   themeColor: "#8B5CF6",
   robots: "index, follow",
   viewport: "width=device-width, initial-scale=1, maximum-scale=1",
   icons: "/Profil.jpg",
+  openGraph: {
+    images: "https://cdn.discordapp.com/attachments/1271835757375393876/1271836150998372392/Wizyx_Code_3.gif?ex=66b8c937&is=66b777b7&hm=b5e6dd9a89eb6c7115bf12e6046954b60d4646095e9040944c3b689a9f0d69c6&",
+  },
 };
 
 export default function RootLayout({
